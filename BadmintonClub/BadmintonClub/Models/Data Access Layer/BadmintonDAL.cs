41586253bb4413261ccdec223bf9e2d7f1a5cd43
Sandeep Singh <sandeep01@hotmail.sg>
2018-02-15 18:01:08 -0800

    s

--- BadmintonClub/BadmintonClub/Models/Data Access Layer/BadmintonDAL.cs
+++ BadmintonClub/BadmintonClub/Models/Data Access Layer/BadmintonDAL.cs
@@ -6,6 +6,9 @@ namespace BadmintonClub.Models.Data_Access_Layer
 {
     public class BadmintonDAL
     {
+        // Server Admin Login {sunny, BadmintonClub2018}
+        private static string connStr = "Server=tcp:badmintonclubdb.database.windows.net,1433;Initial Catalog=BadmintonClubDB;Persist Security Info=False;User ID=sunny;Password=BadmintonClub2018;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
+        
     }
 }
